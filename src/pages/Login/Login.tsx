289ecf41a49210bb6ec6bab4ae1157973257e161
Danila Avetisyan <avetisyandanila21@yandex.ru>
2024-01-29 21:53:40 +0300

    redux jwt

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -7,6 +7,9 @@ import { FormEvent, useState } from "react";
 import axios, { AxiosError } from "axios";
 import { PREFIX } from "../../helpers/API.ts";
 import { LoginResponse } from "../../interfaces/auth.interface.ts";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../store/store.ts";
+import { userActions } from "../../store/user.slice.ts";
 
 export type LoginForm = {
   email: {
@@ -20,6 +23,7 @@ export type LoginForm = {
 export function Login() {
   const [error, setError] = useState<string | null>();
   const navigate = useNavigate();
+  const dispatch = useDispatch<AppDispatch>();
 
   const submit = async (e: FormEvent) => {
     e.preventDefault();
@@ -36,7 +40,7 @@ export function Login() {
         password,
       });
 
-      localStorage.setItem("jwt", data.access_token);
+      dispatch(userActions.addJwt(data.access_token));
       navigate("/");
     } catch (e) {
       if (e instanceof AxiosError) {
